0e4b90f9388f47d7e61944fd75ddc887e8af0182
developerPapai <prechorus.melodies@gmail.com>
2026-04-22 22:34:53 +0530

    deployment error

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://your-backend.onrender.com/api',
+    production: true,
+    apiUrl: 'https://portfolio-backend-dun-gamma-27.vercel.app/api',
 };
